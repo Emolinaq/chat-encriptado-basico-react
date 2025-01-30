2877af1db3a184af2dcc2a08c6b5bab611ed5a30
Emolinaq <molinaquinteroenrique@gmail.com>
2025-01-30 12:18:57 -0400

    - Mostrar los mensajes encriptados en texto legible.  - Añade un botón para desencriptar mensajes seleccionados.

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
+import CryptoJS from 'crypto-js';
 
-const MessageList = ({ messages }) => {
+
+
+const MessageList = ({ messages, isEncrypted, secretKey }) => {
+  const handleDecrypt = (encryptedMessage) => {
+    const bytes = CryptoJS.AES.decrypt(encryptedMessage, secretKey);
+    return bytes.toString(CryptoJS.enc.Utf8);
+  };
+
   return (
     <div className="message-list">
       {messages.map((msg, index) => (
         <div key={index} className={`message ${msg.sender}`}>
-          <p>{msg.text}</p>
+          <p>
+            {isEncrypted ? handleDecrypt(msg.text) : msg.text}
+            </p>
+          {isEncrypted && (
+      <button onClick={() => alert(handleDecrypt(msg.text))}>
+        Desencriptar
+      </button>
+    )}
           <small>{msg.timestamp}</small>
         </div>
       ))}
     </div>
   );
 };
+
 
 export default MessageList;
