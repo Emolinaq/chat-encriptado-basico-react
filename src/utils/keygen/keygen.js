7ac383490efc93cdd426287c06713597a65e0823
Emolinaq <molinaquinteroenrique@gmail.com>
2025-01-31 00:35:50 -0400

    - Generador de key para el uso de la aplicacion.

---
+++ src/utils/keygen/keygen.js
@@ -0,0 +1,20 @@
+const crypto = require('crypto');
+
+// Generar clave secreta de 32 bytes para AES-256
+const secretKey = crypto.randomBytes(32).toString('hex');
+
+// Generar vector de inicialización de 16 bytes
+const iv = crypto.randomBytes(16).toString('hex');
+
+console.log(`Clave Secreta: ${secretKey}`);
+console.log(`Vector de Inicialización: ${iv}`);
+
+//Ejecutar en el terminal :
+// node generateKeys.js para que genere las claves que se usaran en el .env
+// copiar las claves en el .env en su respectivo valor.
+
+//claves de prueba:
+
+//Clave Secreta: 4f3c2a1b5e6d7f8e9a0b1c2d3e4f5g6h7i8j9k0l1m2n3o4p
+//Vector de Inicialización: 1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p
+
